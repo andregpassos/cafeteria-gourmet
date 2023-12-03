41f644f4d764b40a3b158743777fe81194d736a1
Andre Passos <andre.gomes.passos@gmail.com>
2023-12-03 18:20:22 -0300

    adiciona keystore e refatora link email

--- app/scenes/contact/Contact.tsx
+++ app/scenes/contact/Contact.tsx
@@ -1,10 +1,9 @@
 import {NativeStackScreenProps} from '@react-navigation/native-stack';
 import React from 'react';
-import {Text, View, Linking, TouchableOpacity, Alert} from 'react-native';
-import {RootStackParamList} from '../../types/types';
-import {colors, styles} from '../../styles/styles';
+import {Linking, Text, TouchableOpacity, View} from 'react-native';
 import {Icon} from 'react-native-paper';
-import Clipboard from '@react-native-clipboard/clipboard';
+import {styles} from '../../styles/styles';
+import {RootStackParamList} from '../../types/types';
 
 type NavigationProps = NativeStackScreenProps<RootStackParamList, 'Contact'>;
 
@@ -37,11 +36,8 @@ export default function Contact({navigation}: NavigationProps) {
   const mensagemWhatsapp =
     'Olá! Quero entrar em contato com a Cafeteria Gourmet.';
   const whatsappLink = `whatsapp://send?text=${mensagemWhatsapp}&phone=${phoneNumberWithCountryCode}`;
-
-  function copyToClipboard() {
-    Clipboard.setString(email);
-    Alert.alert('E-mail copiado!');
-  }
+  const titleEmail = 'Entrando em contato via app';
+  const emailLink = `mailto:${email}?subject=${titleEmail}`;
 
   return (
     <View
@@ -66,7 +62,7 @@ export default function Contact({navigation}: NavigationProps) {
       />
       <ContactButton
         title={emailText}
-        onPress={copyToClipboard}
+        onPress={() => Linking.openURL(emailLink)}
         icon="email"
         color="red"
         content={email}
